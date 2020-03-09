c31bbf5fd7cb760b7dd0456d13ca6606fa33969e
Aidan Montare <aidan@aidanmontare.net>
2020-03-09 00:57:25 -0400

    first version of collection code

--- collect.py
+++ collect.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Collects WWV Multipath data from Rigol DS1000E series oscilloscopes.
+Collects raw data of channels 1 and 2 from Rigol DS1000E series oscilloscopes.
 
+Also records a metadata file with timescale and voltagescale
+information to allow other programs to interpret the raw data.
 
-Created on Wed Mar  4 14:08:11 2020
+Do not adjust the oscilloscope controls while the program is running.
 
 @author: Aidan Montare <aam141@case.edu>
 
@@ -15,9 +17,29 @@ https://www.cibomahto.com/2010/04/controlling-a-rigol-oscilloscope-using-linux-a
 
 import pyvisa
 import sys
+import time
+import datetime
+import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plot
+
+def readWaveform(scope, channel):
+    """
+    Read the raw waveform from the given scope and channel
+    
+    scope: the opened scope resource
+    channel: an integer channel number
+    
+    returns: the raw data
+    """
+    scope.write(":STOP")
+    
+    scope.write(":WAV:DATA? CHAN" + str(channel))[10:]
+    rawdata = scope.read_raw()
+    
+    scope.write(":RUN")
+    return rawdata
 
 rm = pyvisa.ResourceManager()
 instruments = rm.list_resources()
@@ -28,59 +50,77 @@ if len(usb) != 1:
 scope = rm.open_resource(usb[0], timeout=20, chunk_size=1024000) # bigger timeout for long mem
 
 print('Device in use:')
-print(scope.query("*IDN?"))
+scope_name = scope.query("*IDN?")
+print(scope_name)
 
-# Grab the raw data from channel 1
-scope.write(":STOP")
- 
-# Get the timescale
-timescale = scope.query_ascii_values(":TIM:SCAL?")[0]
-
-# Get the timescale offset
-timeoffset = scope.query_ascii_values(":TIM:OFFS?")[0]
-voltscale = scope.query_ascii_values(':CHAN1:SCAL?')[0]
-
-# And the voltage offset
-voltoffset = scope.query_ascii_values(":CHAN1:OFFS?")[0]
+# configure scope
 
 scope.write(":WAV:POIN:MODE RAW")
-rawdata = scope.query_binary_values(":WAV:DATA? CHAN1")[10:]
-data_size = len(rawdata)
-sample_rate = scope.query_ascii_values(':ACQ:SAMP?')[0]
-print('Data size:', data_size, "Sample rate:", sample_rate)
 
-scope.write(":RUN")
-scope.close()
+# Get metadata
 
-data = np.array(rawdata)
+timescale = scope.query_ascii_values(":TIM:SCAL?")[0]
+timeoffset = scope.query_ascii_values(":TIM:OFFS?")[0]
 
-# Walk through the data, and map it to actual voltages
-# This mapping is from Cibo Mahto
-# First invert the data
-data = data * -1 + 255
- 
-# Now, we know from experimentation that the scope display range is actually
-# 30-229.  So shift by 130 - the voltage offset in counts, then scale to
-# get the actual voltage.
-data = (data - 130.0 - voltoffset/voltscale*25) / 25 * voltscale
+voltscale = [scope.query_ascii_values(':CHAN1:SCAL?')[0],
+             scope.query_ascii_values(':CHAN2:SCAL?')[0]]
+voltoffset = [scope.query_ascii_values(":CHAN1:OFFS?")[0],
+              scope.query_ascii_values(":CHAN2:OFFS?")[0]]
 
-# Now, generate a time axis.
-time = np.linspace(timeoffset - 6 * timescale, timeoffset + 6 * timescale, num=len(data))
- 
-# See if we should use a different time axis
-if (time[-1] < 1e-3):
-    time = time * 1e6
-    tUnit = "uS"
-elif (time[-1] < 1):
-    time = time * 1e3
-    tUnit = "mS"
-else:
-    tUnit = "S"
- 
-# Plot the data
-plot.plot(time, data)
-plot.title("Oscilloscope Channel 1")
-plot.ylabel("Voltage (V)")
-plot.xlabel("Time (" + tUnit + ")")
-plot.xlim(time[0], time[-1])
-plot.show()
+samplerate = scope.query_ascii_values(':ACQ:SAMP?')[0]
+
+metadata = {"timescale": timescale, "timeoffset": timeoffset,
+            "voltscale": voltscale, "voltoffset": voltoffset,
+            "samplerate": samplerate}
+
+
+# the last datetime for which we recorded data
+lastSecondRecorded = (datetime.datetime.now(datetime.timezone.utc)
+                     .replace(microsecond=0))
+
+try:
+    # The main loop runs continuously, but useful code is only
+    # executed if the current UTC second is more than half over
+    # and we have not recorded data for this second yet.
+    # This translates to recording data once a second at the half-
+    # second mark.
+    while True:
+        now = datetime.datetime.now(datetime.timezone.utc)
+        
+        if now > now.replace(microsecond=500000) and \
+        now.replace(microsecond=0) > lastSecondRecorded:
+            print("recording data for", now.isoformat(), end="\t[")
+            lastSecondRecorded = now.replace(microsecond=0)
+            
+            # read and record data for each channel
+            for channel in [1, 2]:
+                rawdata = readWaveform(scope, channel)
+                filename = now.strftime("%Y-%m-%dT%H-%M-%S%Z") + \
+                    "chan" + str(channel) + ".bin"
+                
+                with open(filename, "wb") as f:
+                    f.write(rawdata)
+                print("*", end="")
+            
+            # check if we ran too long
+            check = datetime.datetime.now(datetime.timezone.utc)
+            if check.replace(microsecond=0) > lastSecondRecorded:
+                print('\nRead operation took too long: possible data errors in the last set of files')
+                sys.exit(-1)
+            
+            metadata_filename = now.strftime("%Y-%m-%dT%H-%M-%S%Z") + ".json"
+            
+            with open(metadata_filename, "w") as f:
+                json.dump(metadata, f, indent=1)
+                f.write("\n")
+            print("*]")
+            
+        else:
+            None
+        time.sleep(.1)
+except KeyboardInterrupt:
+    # If the user interrupts the program, end gracefully.
+    print("Ending collection...", end="")
+    scope.close()
+    print("Scope closed.")
+    
